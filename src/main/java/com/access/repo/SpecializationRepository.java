07e0a8e81ec9a4c7f9cee051b605df00ecae101b
Mahant <MahantR@michigan.gov>
2025-08-26 22:16:34 -0500

    Specialization Repo

---
+++ src/main/java/com/access/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package com.access.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.access.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
