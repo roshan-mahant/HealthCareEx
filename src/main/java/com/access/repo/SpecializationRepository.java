9e202523e695c858f6438a7e7985410cef611cff
MahantR <MahantR@T6084F8RHDK3.som.ad.state.mi.us>
2025-08-27 10:37:01 -0500

    "Testing Eclipse Git"

--- src/main/java/com/access/repo/SpecializationRepository.java
+++ src/main/java/com/access/repo/SpecializationRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.access.entity.Specialization;
 
+//repo
 public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
 
 }
