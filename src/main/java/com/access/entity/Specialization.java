69ec9e6ea37e3c2a276414b83a19549105830d53
Mahant <MahantR@michigan.gov>
2025-08-26 22:05:20 -0500

    first commit

---
+++ src/main/java/com/access/entity/Specialization.java
@@ -0,0 +1,30 @@
+package com.access.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.Id;
+
+@Data
+@Entity
+@Table(name="specialization_tab")
+public class Specialization {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="spec_id_col")
+	private Long Id;
+	
+	@Column(name="spec_code_col")
+	private String specCode;
+	
+	@Column(name="spec_name_col")
+	private String specName;
+	
+	@Column(name="spec_note_col")
+	private String specNote;
+
+}
